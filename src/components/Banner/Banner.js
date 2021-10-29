61e1e66a19ab22999664c1156e91f510c2ea7d3e
sahilkandpal <sahilkandpal2003@gmail.com>
2021-10-29 09:42:20 +0530

    delete assets

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,5 @@
 import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
-import flowerwebm from "../../assets/videos/flower.webm";
-import flowermp4 from "../../assets/videos/flower-mp4.mp4";
 const Banner = () => {
   return (
     <section className="relative add-overlay" style={{ height: "34rem" }}>
@@ -13,8 +11,14 @@ const Banner = () => {
         className="w-full h-full object-none"
         style={{ objectPosition: "50% 30%" }}
       >
-        <source type="video/webm" src={flowerwebm} />
-        <source type="video/mp4" src={flowermp4} />
+        <source
+          type="video/webm"
+          src="https://cdn2.hubspot.net/hubfs/1616824/Snapshot%20loop%20V2-MP4.webm"
+        />
+        <source
+          type="video/mp4"
+          src="https://cdn2.hubspot.net/hubfs/1616824/Snapshot%20loop%20V2-MP4.mp4"
+        />
       </video>
       <div className="w-full md:w-160 mx-auto absolute z-10 top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2">
         <div className="text-4xl px-7 md:px-0 font-bold text-white mb-10">
